e9aa7b0aa5562b765ecc88cd49378a97cc7b1f8e
Matias Acevedo <matiasacevedoivan@gmail.com>
2020-10-20 19:48:08 -0300

    empty fragments were added

---
+++ app/src/main/java/project/socialbee/view/ui/fragments/MapDetailDialogFragment.kt
@@ -0,0 +1,25 @@
+package project.socialbee.view.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class MapDetailDialogFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+}
