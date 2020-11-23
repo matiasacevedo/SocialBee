298d4318bc2f0319bac3eeafbb82129ceebb3e85
Matias Acevedo <matiasacevedoivan@gmail.com>
2020-11-23 11:24:42 -0300

    model general data

---
+++ app/src/main/java/project/socialbee/view/model/GeneralData.kt
@@ -0,0 +1,10 @@
+package project.socialbee.view.model
+
+class GeneralData {
+    var title = ""
+    var photo = ""
+    var latitude = ""
+    var longitude = ""
+    var description = ""
+    var user = ""
+}
